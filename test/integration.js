35c8542abbe65aa3d9e0f547dc8e1aaca899abd7
ForbesLindesay <forbes@lindesay.co.uk>
2013-07-11 16:25:30 +0100

    Better logging for monthly integration tests

--- test/integration.js
+++ test/integration.js
@@ -1,11 +1,16 @@
 if (process.argv[2] !== 'if-travis' || process.env.CI) {
   var pipermail = require('../')
   var stream = pipermail('https://mail.mozilla.org/pipermail/es-discuss/')
-
+  var month = ''
+  console.log('Integration testing for each month:\n')
   stream
     .on('error', function (e) { throw e })
     .on('data', function (message) {
-      console.dir(message.url)
+      var m = /\/(\d\d\d\d-[A-Z][a-z]*)\//.exec(message.url)[1]
+      if (m !== month) {
+        console.log(' - ' + m)
+        month = m
+      }
     })
     .on('end', function () {
       console.log('DONE')
