9dfd43ce7fe0e574c32d9da3b40fdf4690ec5369
ForbesLindesay <forbes@lindesay.co.uk>
2013-07-12 13:30:13 +0100

    Support specifying a maximum number of months to read

--- test/integration.js
+++ test/integration.js
@@ -1,18 +1,18 @@
-if (process.argv[2] !== 'if-travis' || process.env.CI) {
-  var pipermail = require('../')
-  var stream = pipermail('https://mail.mozilla.org/pipermail/es-discuss/')
-  var month = ''
-  console.log('Integration testing for each month:\n')
-  stream
-    .on('error', function (e) { throw e })
-    .on('data', function (message) {
-      var m = /\/(\d\d\d\d-[A-Z][a-z]*)\//.exec(message.url)[1]
-      if (m !== month) {
-        console.log(' - ' + m)
-        month = m
-      }
-    })
-    .on('end', function () {
-      console.log('DONE')
-    })
-}
+var months = Infinity
+if (process.argv[2]) months = +process.argv[2]
+var pipermail = require('../')
+var stream = pipermail('https://mail.mozilla.org/pipermail/es-discuss/', {months: months})
+var month = ''
+console.log('Integration testing for ' + (months === Infinity ? 'all' : months) + ' month' + (months > 1 ? 's': '') + ':\n')
+stream
+  .on('error', function (e) { throw e })
+  .on('data', function (message) {
+    var m = /\/(\d\d\d\d-[A-Z][a-z]*)\//.exec(message.url)[1]
+    if (m !== month) {
+      console.log(' - ' + m)
+      month = m
+    }
+  })
+  .on('end', function () {
+    console.log('\nDONE')
+  })
