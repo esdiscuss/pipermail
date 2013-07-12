ebf1346be1167e1e6c3620283fa8362eb5edacb8
ForbesLindesay <forbes@lindesay.co.uk>
2013-07-12 21:51:21 +0100

    Allow parallelism to be configured

--- test/integration.js
+++ test/integration.js
@@ -1,9 +1,10 @@
-var months = Infinity
+var months = Infinity, parallel = 10
 if (process.argv[2]) months = +process.argv[2]
+if (process.argv[3]) parallel = +process.argv[3]
 var pipermail = require('../')
-var stream = pipermail('https://mail.mozilla.org/pipermail/es-discuss/', {months: months})
+var stream = pipermail('https://mail.mozilla.org/pipermail/es-discuss/', {months: months, parallel: parallel})
 var month = ''
-console.log('Integration testing for ' + (months === Infinity ? 'all' : months) + ' month' + (months > 1 ? 's': '') + ':\n')
+console.log('Integration testing for ' + (months === Infinity ? 'all' : months) + ' month' + (months > 1 ? 's': '') + ' with ' + parallel + ' downloads in parallel:\n')
 stream
   .on('error', function (e) { throw e })
   .on('data', function (message) {
